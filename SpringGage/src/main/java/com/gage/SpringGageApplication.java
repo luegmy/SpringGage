34ea9b4564710cc418c3f37b3b2490679dbc75ef
PC-LENOVO <PC-LENOVO@192.168.1.8>
2019-05-22 23:41:30 -0500

    Primer commit

---
+++ SpringGage/src/main/java/com/gage/SpringGageApplication.java
@@ -0,0 +1,13 @@
+package com.gage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGageApplication.class, args);
+	}
+
+}
